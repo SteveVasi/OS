67dd824288e0002ed3066ba2f0345ab2d5e3a9fc
stefan <64739595+SteveVasi@users.noreply.github.com>
2024-10-25 14:23:24 +0200

    added more argument parsing

--- Makefile
+++ Makefile
@@ -3,12 +3,13 @@
 CC = gcc
 FLAGS = -std=c99 -Wall -pedantic -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_SVID_SOURCE -D_POSIX_C_SOURCE=200809L -g
 
+all: mygrep
 
-main: main.o list.o
-        $(CC) $(FLAGS) -o $@ $^
+mygrep: mygrep.o
+	$(CC) $(FLAGS) -o $@ $^
 
 %.o: %.c %.h
-        $(CC) $(FLAGS) -c -o $@ $<
+	$(CC) $(FLAGS) -c -o $@ $<
 
 clean:
-        rm -f *.o main
+	rm -f *.o main
--- mygrep.c
+++ mygrep.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
+
+#define EXIT_FAILURE -1
+
+extern int optind; // removes inline error in vscode
 
 int main(int argc, char *argv[]){
 
     int opt;
+    char *input;
+    
 
     while(( opt = getopt(argc, argv, "io:")) != -1){
         switch (opt)
@@ -16,7 +23,26 @@ int main(int argc, char *argv[]){
         }
     }
 
+    int pos_args_count = argc - optind;
 
+    if (pos_args_count < 2)
+    {
+        usage();
+        return;
+    }
+    
+    input = argv[optind];
+
+    if ( (argc - optind) == 2 ) { /* number of positional arguments is 2 */
+        char *input = argv[optind];
+        char *output = argv[optind+1];
+    } else { /* error handling */}
+
+
+}
+
+void usage(void){
+    exit(EXIT_FAILURE);
 }
 
 
