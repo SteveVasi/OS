47b3f3a9dd0e4f9be9cd4aed29911139e6705adc
stefan <64739595+SteveVasi@users.noreply.github.com>
2024-10-26 17:35:23 +0200

    added more impl

--- mygrep.c
+++ mygrep.c
@@ -1,49 +1,139 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
+#include <string.h>
+#include <bits/getopt_core.h>
 
-#define EXIT_FAILURE -1
+#define bool int
+#define true 1
+#define false 0
+#define BUFFER_MAX 2000
 
-extern int optind; // removes inline error in vscode
+// forward declarations
+void parseFlags(int argc, char** argv);
+FILE* findOutput();
+void usage(void);
+FILE* findInput(int argc, char** argv);
+char* findKeyword(char** argv);
+void fopenFail(void);
+
+// flags
+bool isCaseSensitive = false;
+bool wantsOutFile = false;
+char* outPath;
+char* inPath;
+
+
 
 int main(int argc, char *argv[]){
 
-    int opt;
-    char *input;
+    parseFlags(argc, argv);
+    FILE* output = findOutput();
+    FILE* input = findInput(argc, argv);
+    char* keyword = findKeyword(argv);
     
 
+    char* currentLine = malloc(BUFFER_MAX * sizeof(char));
+    bool hasNext;
+
+    do {
+        // strstr and strcasestr for comparison. tolower toupper
+        hasNext = fgets(currentLine, BUFFER_MAX, input) != NULL;
+        bool (*compare)(char*, char*);
+
+        if (isCaseSensitive)
+        {
+            /* code */
+        } else{
+            
+        }
+        
+    } 
+    while (hasNext); 
+    
+    
+    free(currentLine);
+}
+
+void parseFlags(int argc, char** argv){
+    int opt;
     while(( opt = getopt(argc, argv, "io:")) != -1){
         switch (opt)
         {
         case 'i':
+            isCaseSensitive = true;
             break;
         case 'o':
+            wantsOutFile = true;
+            outPath = optarg;
             break; 
         default:
+            usage();
             break;
         }
     }
+}
 
-    int pos_args_count = argc - optind;
-
-    if (pos_args_count < 2)
+FILE* findOutput(){
+    if (wantsOutFile)
     {
-        usage();
-        return;
+        FILE* outfile = fopen(outPath, "w");
+
+        if (outfile == NULL)
+        {
+            fopenFail();
+        }
+        
+        return outfile;
+    } else{
+        return stdout;
     }
-    
-    input = argv[optind];
-
-    if ( (argc - optind) == 2 ) { /* number of positional arguments is 2 */
-        char *input = argv[optind];
-        char *output = argv[optind+1];
-    } else { /* error handling */}
-
 
 }
 
+FILE* findInput(int argc, char** argv){
+    int pos_args_count = argc - optind;
+
+    if (pos_args_count < 1)
+    {
+        usage();
+    }
+
+    if (pos_args_count == 1)
+    {
+        return stdin;
+    }
+
+    if ( (argc - optind) == 2 ) { /* number of positional arguments is 2 */
+        inPath = argv[optind+1];
+        FILE* input = fopen(inPath, "r");
+
+        if (input == NULL)
+        {
+            fopenFail();
+        }
+
+        return input;
+        
+    } else { 
+        printf("Not implemented yet!");
+        exit(EXIT_FAILURE);
+    }
+}
+
+char* findKeyword(char** argv){
+    return argv[optind];
+}
+
 void usage(void){
+    fprintf(stderr, "Usage Message");
+    exit(EXIT_FAILURE);
+}
+
+void fopenFail(void){
+    fprintf(stderr, "fopen failed: %s\n", strerror(errno));
     exit(EXIT_FAILURE);
 }
 
 
-// strstr and strcasestr for comparison. tolower toupper
