83d473cae430aacbb95639b75dbca9c5728309c1
stefan <64739595+SteveVasi@users.noreply.github.com>
2024-11-02 20:37:44 +0100

    added getopt

--- e2/supervisor.c
+++ e2/supervisor.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <getopt.h>
 
 void usage(void);
 
 int main(int argc, char** argv){
+    int limit = INT_MAX;
+    int delay = 0;
+    int opt;
+    while ((opt = getopt(argc, argv, "n:w:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'n':
+            limit = atoi(optarg);
+            break;
+        case 'w':
+            delay = atoi(optarg);
+            break;
+        default:
+            usage();
+            exit(EXIT_FAILURE);
+            break;
+        }
+    }
+    
+
     usage();
 }
 
 void usage(void){
-    printf("Hello World!");
+    printf("SYNOPSIS:\n");
+    printf("    supervisor [-n limit] [-w delay]\n");
 }
