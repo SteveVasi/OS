9f9a22159e2531c1c855cc577b214c923de2a814
SteveVasi <64739595+SteveVasi@users.noreply.github.com>
2025-01-18 16:33:31 +0100

    added more comments and part of impl

--- e2/forksort.c
+++ e2/forksort.c
@@ -1,5 +1,9 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 
 #define bool int
 #define errorCode bool
@@ -22,7 +26,7 @@ int getMidpoint(int size){
     {
         return size / 2;
     } else {
-        size / 2 + 1;
+        return size / 2 + 1;
     }
 }
 
@@ -34,6 +38,14 @@ int getMidpoint(int size){
  * 
  */
 int main(int argc, char *argv[]) {
+
+    /*
+        use fflush after printf to avoid weird behaviour
+    
+        gdb debugging:
+            before fork is executed: set follow-fork-mode [child|parent]
+    
+     */
 
     int length = get_input_length(argc);
 
@@ -48,16 +60,56 @@ int main(int argc, char *argv[]) {
     }
 
     if(length > 1){
-        //split into two parts with length n/2
-        // if odd, then one part is longer
         int midpoint = getMidpoint(length);
+
         
-        // use fork and execlp to:
         // execute each part in a new child process (2 new child processes are created)
         
-        
-        
-        // create 2 unnamed pipes per child to redirect stdin and stdout with dup2
+        pid_t pid_first_child = fork();
+        pid_t pid_second_child = fork();
 
+        // create 2 unnamed pipes per child to redirect stdin and stdout with dup2
+        // [0] is read end [1] is write end
+        int pipefd_first_child[2];
+        int pipefd_second_child[2];
+        
+        if (pipe(pipefd_first_child) == -1) {
+               perror("pipe error");
+               exit(EXIT_FAILURE);
+        }
+        if (pipe(pipefd_second_child) == -1) {
+               perror("pipe error");
+               exit(EXIT_FAILURE);
+        }
+
+
+
+        switch (pid_first_child)
+        {
+        case -1:
+            perror("Could not fork!\n");
+            break;
+        case 0: //child task
+            //exec
+        
+        
+            //execlp: l -> variable number of args, 
+            //        p -> search env var $PATH for the program specified 
+            // 1. argument is programs name argv[0]
+            // 2. argument is NULL pointer
+            
+            //exit
+            break;
+        default: //parent task
+            // parent wants to use childs output
+            // redirect stdin (fd 0, stdin_fileno)
+            // and or (fd 1, stout_fileno) in new process
+            //wait for child to finish
+            break;
+        }
+        
+
+
+        //exit with exit(EXIT_FAILURE/SUCCESS)
     }
 }
